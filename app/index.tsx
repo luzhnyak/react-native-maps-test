d19df0f138b0ce30b5f5fa74a2bc1d8e79c5c424
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2025-02-02 13:07:34 +0200

    create expo app

---
+++ app/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={StyleSheet.absoluteFillObject}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker coordinate={{ latitude: 37.78825, longitude: -122.4324 }}>
+          <Callout>
+            <Text>Hello World!</Text>
+          </Callout>
+        </Marker>
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
